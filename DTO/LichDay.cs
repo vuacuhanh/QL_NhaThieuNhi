2277114032e8b7b1a545ad6e050edd16ed282545
tranphu112 <tranhuuphu1598@gmail.com>
2024-11-25 18:02:31 +0700

    ql_hocvien

--- DTO/LichDay.cs
+++ DTO/LichDay.cs
@@ -4,7 +4,6 @@ namespace DTO
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("LichDay")]
     public partial class LichDay
